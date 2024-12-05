691312f56d8cc63da0e63e3e5ea65d692249f23c
soyDESKTOP68A2MRFT <86683930+soyDESKTOP68A2MRFT@users.noreply.github.com>
2024-12-05 08:51:10 -0500

    Update combine.sh

--- Combine-Earpods/combine.sh
+++ Combine-Earpods/combine.sh
@@ -9,7 +9,7 @@ fi
 # Función para sincronizar audífonos
 sync_audifonos() {
   echo "Sincronizando los audífonos..."
-  pactl load-module module-combine-sink slaves=bluez_sink.41_42_B0_38_00_23.a2dp_sink,bluez_sink.41_42_6C_C2_F6_4A.a2dp_sink
+  pactl load-module module-combine-sink slaves=bluez_sink.<MAC>.a2dp_sink,bluez_sink.<MAC>.a2dp_sink
   if [ $? -eq 0 ]; then
     echo "Audífonos sincronizados con éxito."
   else
@@ -21,21 +21,21 @@ sync_audifonos() {
 # Función para conectar audífonos
 connect_audifonos() {
   echo "Intentando conectar los audífonos..."
-  bluetoothctl connect 41:42:B0:38:00:23
+  bluetoothctl connect <MAC>
   if [ $? -eq 0 ]; then
     echo "Primer audífono conectado. Aplicando perfil A2DP..."
     sleep 5
-    pactl set-card-profile bluez_card.bluez_sink.41_42_B0_38_00_23 a2dp_sink
+    pactl set-card-profile bluez_card.bluez_sink.<MAC> a2dp_sink
   else
     echo "Error al conectar el primer audífono."
     exit 1
   fi
 
-  bluetoothctl connect 41:42:6C:C2:F6:4A
+  bluetoothctl connect <MAC>
   if [ $? -eq 0 ]; then
     echo "Segundo audífono conectado. Aplicando perfil A2DP..."
     sleep 5
-    pactl set-card-profile bluez_card.bluez_sink.41_42_6C_C2_F6_4A a2dp_sink
+    pactl set-card-profile bluez_card.bluez_sink.<MAC> a2dp_sink
   else
     echo "Error al conectar el segundo audífono."
     exit 1
@@ -47,8 +47,8 @@ connect_audifonos() {
 # Función para desconectar audífonos
 disconnect_audifonos() {
   echo "Intentando desconectar los audífonos..."
-  bluetoothctl disconnect 41:42:B0:38:00:23
-  bluetoothctl disconnect 41:42:6C:C2:F6:4A
+  bluetoothctl disconnect <MAC>
+  bluetoothctl disconnect <MAC>
   if [ $? -eq 0 ]; then
     echo "Audífonos desconectados con éxito."
   else
@@ -73,11 +73,11 @@ unsync_audifonos() {
 fix_audifonos() {
   echo "Cambiando perfiles de los audífonos..."
 
-  pactl set-card-profile bluez_card.41_42_B0_38_00_23 handsfree_head_unit
-  pactl set-card-profile bluez_card.41_42_B0_38_00_23 a2dp_sink
+  pactl set-card-profile bluez_card.<MAC> handsfree_head_unit
+  pactl set-card-profile bluez_card.<MAC> a2dp_sink
 
-  pactl set-card-profile bluez_card.41_42_6C_C2_F6_4A handsfree_head_unit
-  pactl set-card-profile bluez_card.41_42_6C_C2_F6_4A a2dp_sink
+  pactl set-card-profile bluez_card.<MAC> handsfree_head_unit
+  pactl set-card-profile bluez_card.<MAC> a2dp_sink
 
   if [ $? -eq 0 ]; then
     echo "Los perfiles de los audífonos se alternaron con éxito."
